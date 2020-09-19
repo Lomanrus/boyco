134b8232941e30deed3ee64a3168e6d300c5d142
Александр Тихонов <oren.andrey@gmail.com>
2020-09-19 15:39:58 +0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <formBlock v-on:addMoney="addMoney" v-on:setBudget="setBudget" v-on:addPayment="addPayment"></formBlock>
+    <div class="wrapper">
+      <h1>Бюджет {{budget}}</h1>
+      <h2>Остаток {{excess}}</h2>
+      <h1 v-if="excess<=0">Израсходован весь бюджет!</h1>
+      <ul>
+        <li v-for="item in allPayments" :key="item.id" :class="item.type">
+          <span>{{ item.name }}</span>
+          <span>{{ item.sum }}</span>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import formBlock from "@/components/form";
 
 export default {
   name: "App",
+  data(){
+    return{
+      allPayments:[],
+      budget:0,
+      excess:0,
+      paymentTemplate:{type:'',name:'',sum:''}
+    }
+  },
   components: {
-    HelloWorld
+    formBlock
+  },
+  methods:{
+    addMoney(data){
+      if(data.operationName && data.operationSum){
+        const item = JSON.parse(JSON.stringify(this.paymentTemplate))
+        item.type = 'add';
+        item.name = data.operationName;
+        item.sum = data.operationSum;
+        this.allPayments.push(item);
+        console.log(this.allPayments)
+        this.budget += +data.operationSum;
+        this.excess += +data.operationSum
+      }
+    },
+    setBudget(operationSum){
+      if (operationSum){
+        this.budget = +operationSum;
+        this.excess = this.budget;
+        this.allPayments=[]
+      }
+    },
+    addPayment(data){
+      if(data.operationName && data.operationSum){
+        const item = JSON.parse(JSON.stringify(this.paymentTemplate))
+        item.type = 'pay';
+        item.name = data.operationName;
+        item.sum = data.operationSum;
+        this.allPayments.push(item);
+        this.excess = this.budget - data.operationSum;
+
+      }
+    }
   }
 };
 </script>
@@ -24,5 +74,41 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 }
+.wrapper{
+  width: 70%;
+}
+ul{
+  width: 50%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+ul li{
+  width: 200px;
+  height: 100px;
+  padding: 20px 0;
+  margin-bottom: 10px;
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #fafafa;
+  font-size: 18px;
+  color: #000;
+  border-radius: 5px;
+  border: 1px solid black;
+}
+.add{
+  background-color: #c3fca0;
+}
+.pay{
+  background-color: #ff9d9d;
+}
+
 </style>
---
+++ src/components/form.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <input type="text" placeholder="Название операции" v-model="operationName">
+    <input type="number" placeholder="Сумма операции" v-model="operationSum">
+    <button @click="setBudget()">Установить бюджет</button>
+    <button @click="addMoney()">Добавить доход</button>
+    <button @click="addPayment()">Добавить расход</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "formBlock",
+  data(){
+    return {
+      operationName: '',
+      operationSum: null
+    }
+  },
+  methods: {
+    addMoney: function () {
+      this.$emit('addMoney', {operationName:this.operationName,operationSum: this.operationSum})
+      this.operationName=''
+      this.operationSum = 0
+    },
+    setBudget() {
+      this.$emit('setBudget', this.operationSum)
+      this.operationName=''
+      this.operationSum = 0
+    },
+    addPayment() {
+      this.$emit('addPayment', {operationName:this.operationName,operationSum: this.operationSum})
+      this.operationName=''
+      this.operationSum = 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+  div{
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+  }
+  input, button{
+    margin: 5px 0;
+    padding: 10px 20px;
+    border: 1px solid blue;
+  }
+</style>
